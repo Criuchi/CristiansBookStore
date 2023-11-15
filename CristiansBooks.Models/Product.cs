8167596ed7adf903ea64656ba9270917d77e673a
W0785530 <W0785530@ADL-A1201H-10>
2023-11-15 18:59:32 -0500

    Creation of Product.cs Class, Validation Changes and new AddValidationToProduct migration creation.

--- CristiansBooks.Models/Product.cs
+++ CristiansBooks.Models/Product.cs
@@ -12,9 +12,15 @@ namespace CristiansBooks.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
+
+        [Required]
         public string ISBN { get; set; }
+
+        [Required]
         public string Author { get; set; }
 
         [Required]
