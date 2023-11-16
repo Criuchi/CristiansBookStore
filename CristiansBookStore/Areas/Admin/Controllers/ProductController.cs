4a2e5b70d76546f16f4dab2ea1b9939790c886e1
W0785530 <W0785530@ADL-A1201G-18>
2023-11-16 17:40:12 -0600

    Creation of ProductController and ProductVM Class.

---
+++ CristiansBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using CristiansBooks.DataAccess.Repository.IRepository;
+using CristiansBooks.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CristiansBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment; //To Upload images on the server inside wwwroot
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CristiansBooks.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CristiansBooks.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set;} // displays all the product details
+
+        public IEnumerable<SelectListItem> CategoryList { get; set; } // install the new feature package
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; }
+    }
+}
