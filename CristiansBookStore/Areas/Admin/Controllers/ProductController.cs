284ee9a7e74ed52ca6c100a03d0352ce0468d67f
W0785530 <W0785530@ADL-A1201G-18>
2023-11-16 18:33:40 -0600

    Creation of index and upsert in Product and creation of the Product.js file.

--- CristiansBookStore/Areas/Admin/Controllers/ProductController.cs
+++ CristiansBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using CristiansBooks.DataAccess.Repository.IRepository;
 using CristiansBooks.Models;
+using CristiansBooks.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,5 +27,84 @@ namespace CristiansBookStore.Areas.Admin.Controllers
         {
             return View();
         }
+
+        
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            if(id == null)
+            {
+                // this is for create
+                return View(productVM);
+            }
+
+            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if(productVM.Product == null)
+            {
+                return NotFound();
+            }
+            return View(productVM);
+        }
+
+        /*
+        public IActionResult Upsert(CoverType coverType)
+        {
+            if (ModelState.IsValid) // checks all the validations in the model
+            {
+                if (coverType.Id == 0)
+                {
+                    _unitOfWork.CoverType.Add(coverType);
+                    _unitOfWork.Save();
+                }
+                else
+                {
+                    _unitOfWork.CoverType.Update(coverType);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index)); // to see all the cover types
+            }
+            return View(coverType);
+        }
+
+        */
+        //API calls here
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            // return NotFound();
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category, CoverType");
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete successful" });
+        }
+        #endregion
     }
 }
