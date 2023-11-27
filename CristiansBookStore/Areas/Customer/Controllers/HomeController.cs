7e6a606e8319d852886a80df5269528a185d8194
W0785530 <W0785530@ADL-A1201H-03>
2023-11-27 17:30:59 -0500

    Finding the solution to my issues.

--- CristiansBookStore/Areas/Customer/Controllers/HomeController.cs
+++ CristiansBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using CristiansBookStore.Models;
+﻿using CristiansBooks.DataAccess.Repository.IRepository;
+using CristiansBooks.Models;
+using CristiansBookStore.Models;
 using CristiansBookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,15 +16,18 @@ namespace CristiansBookStore.Area.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unifOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unifOfWork)
         {
             _logger = logger;
+            _unifOfWork = unifOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unifOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
